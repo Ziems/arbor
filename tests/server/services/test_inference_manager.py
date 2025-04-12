90baf579f11926c786a2309894441caf82c1272a
Liheng Lai <liheng@future-hgx-2.stanford.edu>
2025-04-11 19:31:11 -0700

    commit before switching cluster

--- tests/server/services/test_inference_manager.py
+++ tests/server/services/test_inference_manager.py
@@ -1,10 +1,4 @@
-import arbor
-print(arbor.__file__)
-
-
-from arbor.server.services.file_manager import FileManager
 from arbor.server.services.inference_manager import InferenceManager
-# import arbor.server.services.inference_manager
 from arbor.server.core.config import Settings
 
 settings = Settings()
