a64c34c5d9fc274215e94fe78ede87f7d38eb19e
noah <nziems2@nd.edu>
2025-05-02 11:45:16 -0400

    Replace vLLM with SGLang

--- src/arbor/server/services/inference_manager.py
+++ src/arbor/server/services/inference_manager.py
@@ -53,14 +53,14 @@ class InferenceManager:
             if model.startswith(prefix):
                 model = model[len(prefix) :]
 
-        print(f"Grabbing a free port to launch an vLLM server for model {model}")
+        print(f"Grabbing a free port to launch an SGLang server for model {model}")
         port = get_free_port()
         timeout = launch_kwargs.get("timeout", 1800)
         my_env = os.environ.copy()
         my_env["CUDA_VISIBLE_DEVICES"] = self.settings.arbor_config.inference.gpu_ids
         n_gpus = self.settings.arbor_config.inference.gpu_ids.count(",") + 1
-        command = f"vllm serve {model} --port {port} --gpu-memory-utilization 0.9 --tensor-parallel-size {n_gpus} --max_model_len 8192 --enable_prefix_caching --guided-decoding-backend xgrammar"
-        # command = f"python -m sglang.launch_server --model-path {model} --port {port} --host 0.0.0.0"
+        # command = f"vllm serve {model} --port {port} --gpu-memory-utilization 0.9 --tensor-parallel-size {n_gpus} --max_model_len 8192 --enable_prefix_caching --guided-decoding-backend xgrammar"
+        command = f"python -m sglang.launch_server --model-path {model} --port {port} --host 0.0.0.0"
         print(f"Running command: {command}")
 
         # We will manually stream & capture logs.
@@ -74,7 +74,7 @@ class InferenceManager:
 
         # A threading.Event to control printing after the server is ready.
         # This will store *all* lines (both before and after readiness).
-        print(f"vLLM server process started with PID {process.pid}.")
+        print(f"SGLang server process started with PID {process.pid}.")
         stop_printing_event = threading.Event()
         logs_buffer = []
 
@@ -88,7 +88,7 @@ class InferenceManager:
                     buffer.append(line)
                     # Print only if stop_event is not set
                     if not stop_event.is_set():
-                        print(f"[vLLM LOG] {line}", end="")
+                        print(f"[SGLang LOG] {line}", end="")
 
         # Start a background thread to read from the process continuously
         thread = threading.Thread(
@@ -126,6 +126,7 @@ class InferenceManager:
         self.current_model = model
 
     def kill(self):
+        from sglang.utils import terminate_process
 
         if self.process is None:
             print("No running server to kill.")
@@ -133,6 +134,8 @@ class InferenceManager:
 
         process = self.process
         thread = self.thread
+
+        terminate_process(process)
 
         # Clear references first
         self.process = None
