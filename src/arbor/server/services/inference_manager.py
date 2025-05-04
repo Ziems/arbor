aa34b56912679d19a05719ddfeba60b0dfd4c94f
noah <nziems2@nd.edu>
2025-05-04 00:51:55 -0400

    Use round robin for sglang_router strategy

--- src/arbor/server/services/inference_manager.py
+++ src/arbor/server/services/inference_manager.py
@@ -60,7 +60,7 @@ class InferenceManager:
         my_env["CUDA_VISIBLE_DEVICES"] = self.settings.arbor_config.inference.gpu_ids
         n_gpus = self.settings.arbor_config.inference.gpu_ids.count(",") + 1
         # command = f"vllm serve {model} --port {port} --gpu-memory-utilization 0.9 --tensor-parallel-size {n_gpus} --max_model_len 8192 --enable_prefix_caching --guided-decoding-backend xgrammar"
-        command = f"python -m sglang_router.launch_server --model-path {model} --dp-size {n_gpus} --router-balance-abs-threshold 4 --port {port} --host 0.0.0.0"
+        command = f"python -m sglang_router.launch_server --model-path {model} --dp-size {n_gpus} --router-policy round_robin --port {port} --host 0.0.0.0"
         print(f"Running command: {command}")
 
         # We will manually stream & capture logs.
