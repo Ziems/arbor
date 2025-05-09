6b887bca88c67887bbad268a50273200f8807ad1
noah <nziems2@nd.edu>
2025-05-09 12:21:43 -0400

    Debug inference issue

--- arbor/server/services/inference_manager.py
+++ arbor/server/services/inference_manager.py
@@ -199,8 +199,15 @@ class InferenceManager:
         url = f"{self.launch_kwargs['api_base']}/chat/completions"
         try:
             self.inference_count += 1
-            response = requests.post(url, json=request_json)
+            self.last_request = request_json
+            response = requests.post(url, json=request_json, timeout=30)
+            self.last_response = response
             return response.json()
+        except requests.exceptions.Timeout:
+            import pdb
+
+            pdb.set_trace()
+            print("Server timed out...ignoring")
         except requests.exceptions.ConnectionError:
             print("Server disconnected...ignoring")
             return None
