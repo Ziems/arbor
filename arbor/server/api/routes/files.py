70bfa665564c57d4274e12ff7bd6c24718f7c5a1
Noah Ziems <nziems2@nd.edu>
2025-08-17 23:06:51 -0700

    Add typing to files.py routes

--- arbor/server/api/routes/files.py
+++ arbor/server/api/routes/files.py
@@ -3,7 +3,7 @@ from typing import Literal
 from fastapi import APIRouter, Body, File, HTTPException, Request, UploadFile
 
 from arbor.server.api.models.schemas import FileModel, PaginatedResponse
-from arbor.server.services.managers.file_manager import FileValidationError
+from arbor.server.services.managers.file_manager import FileManager, FileValidationError
 
 # https://platform.openai.com/docs/api-reference/files/list
 router = APIRouter()
@@ -15,7 +15,7 @@ async def upload_file(
     file: UploadFile = File(...),
     purpose: Literal["assistants", "vision", "fine-tune", "batch"] = Body("fine-tune"),
 ):
-    file_manager = request.app.state.file_manager
+    file_manager: FileManager = request.app.state.file_manager
     if not file.filename.endswith(".jsonl"):
         raise HTTPException(status_code=400, detail="Only .jsonl files are allowed")
 
@@ -34,7 +34,7 @@ async def upload_file(
 
 @router.get("", response_model=PaginatedResponse[FileModel])
 def list_files(request: Request):
-    file_manager = request.app.state.file_manager
+    file_manager: FileManager = request.app.state.file_manager
     return PaginatedResponse(
         items=file_manager.get_files(),
         total=len(file_manager.get_files()),
@@ -45,12 +45,12 @@ def list_files(request: Request):
 
 @router.get("/{file_id}", response_model=FileModel)
 def get_file(request: Request, file_id: str):
-    file_manager = request.app.state.file_manager
+    file_manager: FileManager = request.app.state.file_manager
     return file_manager.get_file(file_id)
 
 
 @router.delete("/{file_id}")
 def delete_file(request: Request, file_id: str):
-    file_manager = request.app.state.file_manager
+    file_manager: FileManager = request.app.state.file_manager
     file_manager.delete_file(file_id)
     return {"message": "File deleted"}
