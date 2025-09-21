4eea7e54d68cc3ec276cec10fa187c16a45d4c3f
Noah Ziems <nziems@mit.edu>
2025-09-21 11:25:36 -0400

    Update banking grpo test

--- examples/banking_grpo.py
+++ examples/banking_grpo.py
@@ -22,7 +22,7 @@ kwargs = dict(
     trust_remote_code=True,
 )
 
-TOP_CLASSES = CLASSES[:10]
+TOP_CLASSES = CLASSES[:]
 
 raw_data = [
     dspy.Example(x, label=CLASSES[x.label]).with_inputs("text")
@@ -32,9 +32,11 @@ raw_data = [
 
 random.Random(42).shuffle(raw_data)
 print(len(TOP_CLASSES), TOP_CLASSES)
-trainset = raw_data[:400]
-valset = raw_data[400:450]
-assert len(valset) > 30
+trainset = raw_data[:1000]
+devset = raw_data[1000:1100]
+testset = raw_data[1100:1200]
+assert len(devset) > 30
+assert len(testset) > 30
 
 # print(trainset[0])
 
@@ -45,15 +47,14 @@ from dspy.clients.lm_local_arbor import ArborProvider
 # Get Arbor server info from init()
 provider = ArborProvider()
 
-student_lm_name = "Qwen/Qwen2.5-1.5B-Instruct"
-# student_lm_name = "Qwen/Qwen2.5-7B-Instruct"
-# student_lm_name = "Qwen/Qwen2.5-14B-Instruct"
-# student_lm_name = "Qwen/Qwen2.5-32B-Instruct"
+#student_lm_name = "Qwen/Qwen2.5-1.5B-Instruct"
+student_lm_name = "Qwen/Qwen2.5-7B-Instruct"
+#student_lm_name = "Qwen/Qwen2.5-14B-Instruct"
+#student_lm_name = "Qwen/Qwen2.5-32B-Instruct"
 student_lm = dspy.LM(
     model=f"openai/arbor:{student_lm_name}",
     provider=provider,
     temperature=1.0,
-    # api_base='http://localhost:7453/v1/',
     api_base=arbor_server_info["base_url"],
     api_key="arbor",
 )
@@ -66,16 +67,17 @@ metric = lambda x, y, trace=None: x.label == y.label
 from dspy.teleprompt.grpo import GRPO
 
 train_kwargs = {
-    "per_device_train_batch_size": 4,
-    "temperature": 0.7,
+    "per_device_train_batch_size": 1,
+    "temperature": 1.0, 
     "beta": 0.02,
-    "learning_rate": 1e-5,
-    "gradient_checkpointing": False,
+    "learning_rate": 5e-6,
+    "gradient_checkpointing": True,
+    "gradient_accumulation_steps": 8,
     "bf16": True,
     "lr_scheduler_type": "constant_with_warmup",
     "max_prompt_length": None,
     "max_completion_length": None,
-    "lora": False,
+    "lora": True,
     "report_to": "none",  # 'wandb'
     "log_completions": False,
 }
@@ -85,8 +87,8 @@ compiler = GRPO(
     num_dspy_examples_per_grpo_step=4,
     num_rollouts_per_grpo_step=4,
     exclude_demos=True,
-    num_train_steps=50,
-    num_threads=8,
+    num_train_steps=20,
+    num_threads=1,
     use_train_as_val=False,
     num_steps_for_val=10,
     train_kwargs=train_kwargs,
@@ -96,13 +98,17 @@ compiler = GRPO(
 classify_ft = compiler.compile(
     student=student_classify,
     trainset=trainset,
-    valset=valset,
+    valset=devset,
 )
 
+import pdb; pdb.set_trace()
+
 evaluate = dspy.Evaluate(
-    devset=valset,
+    devset=testset,
     metric=metric,
     display_progress=True,
     display_table=5,
     num_threads=16,
 )
+
+import pdb; pdb.set_trace()
